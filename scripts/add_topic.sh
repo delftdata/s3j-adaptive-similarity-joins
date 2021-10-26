bf442a70f9c65e163d3764ca94e168737fcb7b7d
Archer6621 <shaad1@live.nl>
2021-10-26 18:02:36 +0200

    Fix the topic scripts

--- scripts/add_topic.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Quickstart: https://kafka.apache.org/quickstart
-# Note that their way of adding topics is missing arguments (namely --partitions and --replica-factor), line below is the correct way
-# You need to set KAFKA_HOME before calling this, uncommment the line below with the path to the kafka folder (replace the three dots)
-#KAFKA_HOME=replacethis
-$KAFKA_HOME/bin/kafka-topics.sh --create --topic my-topic --bootstrap-server localhost:9092 --partitions 1 --replica-factor 1
--- scripts/add_topic_local.sh
+++ scripts/add_topic_local.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-${KAFKA_HOME}/bin/kafka-topics.sh --create --topic my-topic --bootstrap-server localhost:9092 --partitions 1 --replica-factor 1
+# Quickstart: https://kafka.apache.org/quickstart
+# Note that their way of adding topics is missing arguments (namely --partitions and --replica-factor), line below is the correct way
+# You need to set KAFKA_HOME before calling this, uncommment the line below with the path to the kafka folder (replace the three dots)
+#KAFKA_HOME=replacethis
+$KAFKA_HOME/bin/kafka-topics.sh --create --topic my-topic --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
