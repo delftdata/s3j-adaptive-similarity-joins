1aa24b40d68edcedd465c1bf4833bcb15a2396b9
Archer6621 <shaad1@live.nl>
2021-10-28 11:11:34 +0200

    Improve/add kafka management scripts

--- scripts/add_topic_local.sh
+++ scripts/add_topic_local.sh
@@ -3,4 +3,4 @@
 # Note that their way of adding topics is missing arguments (namely --partitions and --replica-factor), line below is the correct way
 # You need to set KAFKA_HOME before calling this, uncommment the line below with the path to the kafka folder (replace the three dots)
 #KAFKA_HOME=replacethis
-$KAFKA_HOME/bin/kafka-topics.sh --create --topic my-topic --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
+$KAFKA_HOME/bin/kafka-topics.sh --create --topic $1 --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
---
+++ scripts/start_kafka_and_zookeeper.sh
@@ -0,0 +1,1 @@
+(trap 'kill 0' SIGINT; $KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties & $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties)
