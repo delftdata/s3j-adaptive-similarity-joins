1b0e1b67f4a79c8f8b8564107f6a96c474d1bf96
Shaad <shaad1@live.nl>
2021-12-06 12:47:16 +0100

    Adapt deployment scripts and configs to coordinator/monitor

--- kubernetes/start-everything.sh
+++ kubernetes/start-everything.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 ./minikube-restart.sh
+
+kubectl create configmap env-config --from-env-file=./environment/.env
+
 ./deploy-minio.sh
 ./deploy-kafka.sh
-./deploy-flask.sh
+./deploy-coordinator.sh
+./deploy-monitor.sh
 
 # Might need a wait loop for MinIO? Unless the service is up immediately
 ./deploy-flink.sh
