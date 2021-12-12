e44fd7c6f4e51b9c1671ffc296d4d7b5ccac848d
Shaad <shaad1@live.nl>
2021-12-12 17:23:18 +0100

    Fix unsaved scripts

--- kubernetes/check-deployments.sh
+++ kubernetes/check-deployments.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 source ./environment/dependencies.env
-STR=$(kubectl get deployments --all-namespaces | awk '{print $3}')
+STR=$($KUBECTL get deployments --all-namespaces | awk '{print $3}')
 if [[ "$STR" == *"0"* ]]; then
   echo "Some deployments are not up yet..."
-  k3s kubectl get deployments --all-namespaces
+  $KUBECTL get deployments --all-namespaces
 else
 	echo "All green!"
 fi
--- kubernetes/port-forward-minio.sh
+++ kubernetes/port-forward-minio.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source ./environment/dependencies.env
-k3s kubectl port-forward service/minio 9000
+$KUBECTL port-forward service/minio 9000
