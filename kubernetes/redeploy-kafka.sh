63f8ac5c5a2c421cee3c69da923e1e261122449e
jorgeSia <georgedrfsia@gmail.com>
2022-01-19 17:16:23 +0100

    Change kafka deployment.

--- kubernetes/redeploy-kafka.sh
+++ kubernetes/redeploy-kafka.sh
@@ -5,5 +5,13 @@ set +o allexport
 
 $KUBECTL delete namespace kafka
 $KUBECTL create namespace kafka
-$KUBECTL create -f 'https://strimzi.io/install/latest?namespace=kafka' -n kafka
+helm repo add strimzi https://strimzi.io/charts/
+helm uninstall strimzi-kafka
+helm install --namespace kafka --version v0.27.1 strimzi-kafka strimzi/strimzi-kafka-operator
 $KUBECTL apply -f deployments/kafka.yaml -n kafka
+
+#Old deployment
+#$KUBECTL delete namespace kafka
+#$KUBECTL create namespace kafka
+#$KUBECTL create -f 'https://strimzi.io/install/latest?namespace=kafka' -n kafka
+#$KUBECTL apply -f deployments/kafka.yaml -n kafka
