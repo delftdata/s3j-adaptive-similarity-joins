ff763e3e4b304b8045b179886a6c216c68ec84fe
jorgeSia <georgedrfsia@gmail.com>
2022-02-03 23:00:23 +0100

    Use correct minio config file for nfs deployment.

--- kubernetes/redeploy-minio-nfs.sh
+++ kubernetes/redeploy-minio-nfs.sh
@@ -10,4 +10,4 @@ $KUBECTL delete pvc data-1-minio-0
 $KUBECTL delete pvc data-1-minio-1
 helm repo add bitnami https://charts.bitnami.com/bitnami
 $KUBECTL create -f persistent-volumes/minio-nfs-pv.yaml
-helm install --version v7.3.0 -f configs/minio-config.yaml minio bitnami/minio
+helm install --version v7.3.0 -f configs/minio-nfs-config.yaml minio bitnami/minio
