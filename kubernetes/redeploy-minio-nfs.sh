3930db0a0f8669ef8745ff1da0cfd21c3104613e
jorgeSia <georgedrfsia@gmail.com>
2022-02-03 21:39:54 +0100

    Deploy minio over nfs.

---
+++ kubernetes/redeploy-minio-nfs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -o allexport
+source ./environment/dependencies.env
+set +o allexport
+
+helm uninstall minio
+$KUBECTL delete pvc data-0-minio-0
+$KUBECTL delete pvc data-0-minio-1
+$KUBECTL delete pvc data-1-minio-0
+$KUBECTL delete pvc data-1-minio-1
+helm repo add bitnami https://charts.bitnami.com/bitnami
+$KUBECTL create -f minio-nfs-pv.yaml
+helm install --version v7.3.0 -f configs/minio-config.yaml minio bitnami/minio
