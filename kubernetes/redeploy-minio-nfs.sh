1e0fb1a5dd5ac26f7ffebcf41395c6bb892fe4ba
jorgeSia <georgedrfsia@gmail.com>
2022-02-04 01:13:54 +0100

    Delete pvs.

--- kubernetes/redeploy-minio-nfs.sh
+++ kubernetes/redeploy-minio-nfs.sh
@@ -8,6 +8,10 @@ $KUBECTL delete pvc data-0-minio-0
 $KUBECTL delete pvc data-0-minio-1
 $KUBECTL delete pvc data-1-minio-0
 $KUBECTL delete pvc data-1-minio-1
+$KUBECTL delete pv minio-nfs-pv-0
+$KUBECTL delete pv minio-nfs-pv-1
+$KUBECTL delete pv minio-nfs-pv-2
+$KUBECTL delete pv minio-nfs-pv-3
 helm repo add bitnami https://charts.bitnami.com/bitnami
 $KUBECTL create -f persistent-volumes/minio-nfs-pv.yaml
 helm install --version v7.3.0 -f configs/minio-nfs-config.yaml minio bitnami/minio
